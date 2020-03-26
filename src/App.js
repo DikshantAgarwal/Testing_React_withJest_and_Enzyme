862f68d8bddb306b208cea91b514c6d6302fcc13
Dikshant Agarwal <DikshantAgarwal@users.noreply.github.com>
2020-03-26 11:50:32 +0530

    initial commit of components without test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-
+import Header from './components/Header/Header';
+import './app.scss';
 
 class App extends Component {
   render() {
     return (
       <div>
-          Test
+          <Header/>
       </div>
     );
   }
